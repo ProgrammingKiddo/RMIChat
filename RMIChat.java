7ed33aa8a64094cb2db6c50e49ea63c4c88e3517
ProgrammingKiddo <programming.edu@protonmail.com>
2018-01-19 21:29:42 +0100

    Actualizada la documentación de los métodos de la clase RMIChat para describir lo que hacen.

--- RMIChat.java
+++ RMIChat.java
@@ -28,6 +28,7 @@ public class RMIChat extends UnicastRemoteObject implements IRMIChat {
     }
 
     /**
+     * Código de inicialización del servidor.
      * @param args El primer parámetro pasado al ejecutar la clase representa la dirección a utilizar por el servidor.
      */
     public static void main(String[] args) throws Exception{
@@ -42,6 +43,7 @@ public class RMIChat extends UnicastRemoteObject implements IRMIChat {
 
     @Override
     /**
+     * Envía el mensaje recibido por un cliente a todos los clientes registrados.
      * @throws RemoteException
      * @param mensaje La cadena de texto que el servidor debe enviar a todos los clientes registrados.
      */
@@ -57,6 +59,7 @@ public class RMIChat extends UnicastRemoteObject implements IRMIChat {
 
     @Override
     /**
+     * Añade un cliente al vector contenedor de clientes del objeto Servidor.
      * @throws RemoteException
      * @param cliente Representa a un cliente remoto que se quiere registrar en el servidor.
      */
